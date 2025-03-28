a8dc0a1c7a1f33e4c8ee834dee3f4a11e064dc0b
Alex Carol <acarolcampre@adobe.com>
2025-03-28 16:13:11 +0100

    Fix lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,13 @@ module.exports = {
   env: {
     browser: true,
   },
+  globals: {
+    WebImporter: 'readonly',
+    window: 'readonly',
+    document: 'readonly',
+    KeyboardEvent: 'readonly',
+    XPathResult: 'readonly',
+  },
   parser: '@babel/eslint-parser',
   parserOptions: {
     allowImportExportEverywhere: true,
@@ -14,5 +21,22 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    camelcase: ['error', { properties: 'never' }], // allow snake_case in properties only
+    'no-console': 'warn', // warn on console statements
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }], // warn on unused variables except those starting with _
+    'no-shadow': ['error', { allow: ['buttonElement'] }], // allow shadowing of buttonElement
+    'no-redeclare': ['error', { builtinGlobals: false }], // allow redeclaring built-in globals
   },
+  overrides: [
+    {
+      files: ['import.js'],
+      rules: {
+        camelcase: 'off',
+        'no-console': 'off',
+        'no-unused-vars': 'off',
+        'no-shadow': 'off',
+        'no-redeclare': 'off',
+      },
+    },
+  ],
 };
